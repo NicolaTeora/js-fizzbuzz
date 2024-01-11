f5bc11dbb42ed00f3369a3ef99a4a23dfd5844b1
NicolaTeora <nikxodia@gmail.com>
2024-01-11 22:24:27 +0100

    feat: add style by js

--- JS/main.js
+++ JS/main.js
@@ -4,10 +4,13 @@ for (i = 1; i <= 100; i++) {
     let fbzz = '';
     if (i % 15== 0) {
         fbzz = 'fizzbuzz';
+        
     } else if (i % 3 == 0) {
         fbzz = 'fizz';
+
     } else if (i % 5 == 0) {
         fbzz = 'buzz';
+
     } else {
         fbzz = i;
     };
