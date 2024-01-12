dfb5a05fb733e81e1a8dc95bec8c48be43baf549
NicolaTeora <nikxodia@gmail.com>
2024-01-12 15:29:02 +0100

    feat : correction js

--- JS/main.js
+++ JS/main.js
@@ -2,15 +2,16 @@
 for (i = 1; i <= 100; i++) {
 
     let fbzz = '';
+    let newStyle = '';
     if (i % 15== 0) {
         fbzz = 'fizzbuzz';
-        
+        newStyle = ' fzzbzz'
     } else if (i % 3 == 0) {
         fbzz = 'fizz';
-
+        newStyle = ' fzz'
     } else if (i % 5 == 0) {
         fbzz = 'buzz';
-
+        newStyle = ' bzz'
     } else {
         fbzz = i;
     };
@@ -18,7 +19,7 @@ for (i = 1; i <= 100; i++) {
     console.log(fbzz);
 
     document.getElementById('box').innerHTML +=`
-    <div class="square">
+    <div class="square ${newStyle}">
         ${fbzz}
     </div>`;
 
